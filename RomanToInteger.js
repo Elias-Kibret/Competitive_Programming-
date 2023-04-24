875902daa19bed8173baa085b7eaebd4983166c9
Elias-Kibret <eliaskibret2013n2@gmail.com>
2023-04-24 12:20:29 +0300

    leeetcode

--- RomanToInteger.js
+++ RomanToInteger.js
@@ -45,7 +45,17 @@ It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 
  */
 
-symbols = {
+var romanToInt = function (s) {
+	value = 0;
+	for (let i = 0; i < s.length; i++) {
+		roman[s[i]] < roman[s[i + 1]]
+			? (value -= roman[s[i]])
+			: (value += roman[s[i]]);
+	}
+	return value;
+};
+
+let roman = {
 	I: 1,
 	V: 5,
 	X: 10,
@@ -54,13 +64,4 @@ symbols = {
 	D: 500,
 	M: 1000,
 };
-
-var romanToInt = function (s) {
-	value = 0;
-	for (let i = 0; i < s.length; i++) {
-		symbols[s[i]] < symbols[s[i + 1]]
-			? (value -= symbols[s[i]])
-			: (value += symbols[s[i]]);
-	}
-	return value;
-};
+console.log(romanToInt("LVIII")); // 58
