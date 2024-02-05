3b594ffc814e0a69b5f0e5294639321fbf0553b7
Elias Kibret <eliaskibret2013n2@gmail.com>
2024-02-05 20:50:12 +0000

    solved :1512_Number_of_Good_Pairs

---
+++ Leetcode_Python/1512_Number_of_Good_Pairs.py
@@ -0,0 +1,43 @@
+class Solution:
+    def numIdenticalPairs(self, nums: list[int]) -> int:
+
+        if len(nums)<1 or len(nums)>100:
+            print("Invalid Input")
+        count=0
+        
+        for index, value in enumerate(nums):
+            for second_index in range(index+1,len(nums)):
+                if value==nums[second_index]:
+                    count+=1
+        return count
+
+
+# Given an array of integers nums, return the number of good pairs.
+
+# A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,1,1,3]
+# Output: 4
+# Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+# Example 2:
+
+# Input: nums = [1,1,1,1]
+# Output: 6
+# Explanation: Each pair in the array are good.
+# Example 3:
+
+# Input: nums = [1,2,3]
+# Output: 0
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 100
+# 1 <= nums[i] <= 100
+
+
+
