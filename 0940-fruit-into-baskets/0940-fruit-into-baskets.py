2822b386a67decb7cac43c77f202928991ed3274
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-06-01 05:10:27 -0500

    Time: 644 ms (76.4%), Space: 24.4 MB (17.11%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.py
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -3,18 +3,21 @@ class Solution:
         # whenever you have a count hashmap
 
         left,max_length=0,0
-        count={}
 
-        for right in range(len(fruits)):
-            count[fruits[right]]=count.get(fruits[right],0)+1
-            
-            while len(count)>2:
-                count[fruits[left]]-=1
-                if count[fruits[left]]==0:
-                    del count[fruits[left]]
+        fruits_count={}
+
+        for r in range(len(fruits)):
+            fruits_count[fruits[r]]=fruits_count.get(fruits[r],0)+1
+
+            while len(fruits_count)>2:
+                fruits_count[fruits[left]]-=1
+                
+                if fruits_count[fruits[left]]==0:
+                    del fruits_count[fruits[left]]
                 left+=1
-            max_length=max(max_length,right-left+1)
+            max_length=max(max_length,r-left+1)
         return max_length
+
 
 
         
