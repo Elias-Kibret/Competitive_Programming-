725e68a26cfa57d079fa7545e8b73373326fa76f
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-06-02 05:48:03 -0500

    Time: 84 ms (46.12%), Space: 53.4 MB (74.29%) - LeetHub

---
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.ts
@@ -0,0 +1,22 @@
+function maxVowels(s: string, k: number): number {
+    const mySet: Set<String>=new Set(['a','e','i','o','u'])
+
+    let max_count=0
+    let current_count=0
+    for (let i=0; i<k;i++){
+        if(mySet.has(s[i]))current_count++
+    }
+    max_count=current_count
+    for(let right=k;right<s.length;right++){
+        if(mySet.has(s[right])){
+            current_count++
+        }
+        if(mySet.has(s[right-k])){
+            current_count--
+        }
+        max_count=Math.max(max_count,current_count)
+    }
+    return max_count
+
+
+};
