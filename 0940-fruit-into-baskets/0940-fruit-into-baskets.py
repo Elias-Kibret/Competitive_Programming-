b3bddcdd5be9f1b5c5095d8916761f7edf1c19b5
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-06-02 06:26:45 -0500

    Time: 678 ms (52.42%), Space: 24 MB (71.17%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.py
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -1,20 +1,20 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        # whenever you have a count hashmap
-
-        left,max_length=0,0
-
         fruits_count={}
+        l=0 
+        
 
-        for right in range(len(fruits)):
-            fruits_count[fruits[right]]=fruits_count.get(fruits[right],0)+1
+        res=0
+
+        for r in range(len(fruits)):
+            fruits_count[fruits[r]]=fruits_count.get(fruits[r],0)+1
 
             while len(fruits_count)>2:
-                fruits_count[fruits[left]]-=1
-                if fruits_count[fruits[left]]==0:
-                    del fruits_count[fruits[left]]
-                left+=1
-            max_length=max(max_length,right-left+1)
-        return max_length
+                fruits_count[fruits[l]]-=1
 
-
+                if fruits_count[fruits[l]]==0:
+                    del fruits_count[fruits[l]]
+                l+=1 
+            res=max(res,r-l+1)
+        return res
+    
