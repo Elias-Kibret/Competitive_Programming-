c71433db02b9e60e18a993053682c590be27cabc
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-06-03 07:54:43 -0500

    Time: 643 ms (32.54%), Space: 23.4 MB (92.4%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -1,5 +1,40 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+
+        prefix_count=defaultdict(int)
+        prefix_count[0]=1
+
+        current_count=0
+
+        nice_array=0
+
+
+        for num in nums:
+            if num%2!=0:
+                current_count+=1
+            if current_count-k in prefix_count:
+                nice_array+=prefix_count[current_count-k]
+            prefix_count[current_count]+=1
+        return nice_array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         prefix_count=defaultdict(int)
         prefix_count[0]=1
 
