6eed3843660fd7040e8058557a81e5a5780c3ac5
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-06-04 06:16:40 -0500

    Time: 32 ms (84.34%), Space: 16.4 MB (98.44%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,17 +1,28 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        count=Counter(s)
-        length=0
-        odd_found=False
-        for freq in count.values():
-            if freq%2==0:
-                length+=freq
+        # count=Counter(s)
+        # length=0
+        # odd_found=False
+        # for freq in count.values():
+        #     if freq%2==0:
+        #         length+=freq
+        #     else:
+        #         length+=freq-1
+        #         odd_found=True
+        # if odd_found:
+        #     length+=1
+        # return length
+
+        ss = set()
+        for letter in s:
+            if letter not in ss:
+                ss.add(letter)
             else:
-                length+=freq-1
-                odd_found=True
-        if odd_found:
-            length+=1
-        return length
+                ss.remove(letter)
+        if len(ss) != 0:
+            return len(s) - len(ss) + 1
+        else:
+            return len(s)
        
        
        
