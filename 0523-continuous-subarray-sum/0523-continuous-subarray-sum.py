d8eba9a3f649acd1dc1288bdcfe35baa28d62d12
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-06-07 20:39:41 -0500

    Time: 795 ms (98.82%), Space: 35.7 MB (92.93%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+
+        # for i in range(len(nums)-1):
+        #     prefix=nums[i]
+        #     for j in range(i+1,len(nums)):
+        #         prefix+=nums[j]
+        #         if prefix%k==0:
+        #             return True
+        # return False
+
+        mod_map={0:-1}
+
+        prefix_sum=0
+
+        for i, n in enumerate(nums):
+            prefix_sum+=n
+
+            r=prefix_sum%k
+
+            if r not in mod_map:
+                mod_map[r]=i
+            elif i-mod_map[r]>1:
+                return True
+        return False
+
+        
