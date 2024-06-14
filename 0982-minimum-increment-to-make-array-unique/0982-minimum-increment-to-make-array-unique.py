15f495dc8561857db0a0d4d50968800171b82c9a
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-06-13 21:04:28 -0500

    Time: 600 ms (87.84%), Space: 30.9 MB (39.86%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.py
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.py
@@ -1,20 +1,51 @@
 class Solution:
     def minIncrementForUnique(self, nums: List[int]) -> int:
-        nums.sort()
-        l,r=0,1
-        count=0
-        while r<len(nums):
-            if nums[l]==nums[r]:
-                nums[r]=nums[r]+1
-                count+=1
-            elif nums[l]>nums[r]:
-                count+=nums[l]-nums[r]+1
-                nums[r]=nums[l]+1
+    #     nums.sort()
+    #     l,r=0,1
+    #     count=0
+    #     while r<len(nums):
+    #         if nums[l]==nums[r]:
+    #             nums[r]=nums[r]+1
+    #             count+=1
+    #         elif nums[l]>nums[r]:
+    #             count+=nums[l]-nums[r]+1
+    #             nums[r]=nums[l]+1
                 
-            r+=1
-            l+=1
+    #         r+=1
+    #         l+=1
         
-        return count
+    #     return count
+    # def minIncrementForUnique(nums):
+        if not nums:
+            return 0
+        nums.sort()
+        moves = 0
+        prev = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] <= prev:
+                moves += prev + 1 - nums[i]
+                prev += 1
+            else:
+                prev = nums[i]
+        return moves
+
+    
+    
+        
+    
+    
+    
+    
+
+    
+        
+            
+            
+        
+            
+    
+    
+
             
 
        
