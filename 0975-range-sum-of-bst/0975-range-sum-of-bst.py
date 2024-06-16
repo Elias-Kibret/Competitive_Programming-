64b33a1066d86c30bbcd185ffdf88743ab61ae01
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-06-16 16:09:37 -0500

    Time: 104 ms (83.88%), Space: 23.6 MB (62.37%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.py
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.py
@@ -6,13 +6,21 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        total=0
+        # total=0
 
-        if root==None:
+        # if root==None:
+        #     return 0
+        # if root.val>=low and root.val<=high:
+        #     total+=root.val
+        # return total+self.rangeSumBST(root.left,low,high)+self.rangeSumBST(root.right,low,high)
+
+        if not root:
             return 0
-        if root.val>=low and root.val<=high:
-            total+=root.val
-        return total+self.rangeSumBST(root.left,low,high)+self.rangeSumBST(root.right,low,high)
+        if root.val>high:
+            return self.rangeSumBST(root.left,low,high)
+        if root.val<low:
+            return self.rangeSumBST(root.right,low,high)
+        return root.val+ self.rangeSumBST(root.left,low,high)+self.rangeSumBST(root.right,low,high)
 
         
         
