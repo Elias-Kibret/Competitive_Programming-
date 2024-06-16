614170c48b515d3a50aed9721efbd47964d2e78d
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-06-16 18:56:53 -0500

    Time: 39 ms (33.31%), Space: 16.4 MB (97.28%) - LeetHub

--- 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.py
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.py
@@ -6,18 +6,33 @@
 #         self.right = right
 class Solution:
     def minDiffInBST(self, root: Optional[TreeNode]) -> int:
-        ls=[]
+        # ls=[]
 
-        def helper(node):
-            if not node:
-                return []
-            return helper(node.left)+[node.val]+helper(node.right)
+        # def helper(node):
+        #     if not node:
+        #         return []
+        #     return helper(node.left)+[node.val]+helper(node.right)
           
-        ls=helper(root)
-        ans=float("inf")
+        # ls=helper(root)
+        # ans=float("inf")
 
-        for i in range(1,len(ls)):
-            ans=min(ans,ls[i]-ls[i-1])
+        # for i in range(1,len(ls)):
+        #     ans=min(ans,ls[i]-ls[i-1])
 
-        return ans
+        # return ans
         
+
+        prev=-float('inf')
+        min_diff=float('inf')
+
+
+        def in_order(node):
+            nonlocal prev,min_diff
+            if not node:
+                return
+            in_order(node.left)
+            min_diff=min(min_diff,node.val-prev)
+            prev=node.val
+            in_order(node.right)
+        in_order(root)
+        return min_diff
