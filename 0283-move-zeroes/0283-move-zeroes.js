77f88366d8309f11a3cadc35a468c7bb6b89bc85
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-06-23 22:45:48 -0500

    Time: 74 ms (67.16%), Space: 53.6 MB (79.02%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let l=0;
+    for(let i in nums){
+        if(nums[l]==0 && nums[i]!=0){
+            let temp=nums[l]
+            nums[l]=nums[i]
+            nums[i]=temp
+            l++;
+        }
+        if(nums[l]!=0){
+            l++
+        }
+    }
+    
+};
