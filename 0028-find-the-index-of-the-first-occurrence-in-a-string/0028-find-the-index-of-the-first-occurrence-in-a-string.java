44320b648b4ac2858d7f055800669f7cbd3e3ff6
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-06-30 21:36:19 -0500

    Time: 0 ms (100%), Space: 41.4 MB (32.96%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,31 +1,30 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-                // Edge case: when needle is an empty string
-        if (needle.isEmpty()) {
+        if (needle=="") {
             return 0;
         }
 
         int lenH = haystack.length();
         int lenN = needle.length();
 
-        // If needle is longer than haystack, it cannot be found
+        
         if (lenN > lenH) {
             return -1;
         }
 
-        // Loop through the haystack with a sliding window of size lenN
+       
         for (int i = 0; i <= lenH - lenN; i++) {
-            // Check if the substring starting from index i matches needle
+        
             int j = 0;
             while (j < lenN && haystack.charAt(i + j) == needle.charAt(j)) {
                 j++;
             }
             if (j == lenN) {
-                return i; // Match found
+                return i; 
             }
         }
 
-        // If needle is not found in haystack
+        
         return -1;
 
         
