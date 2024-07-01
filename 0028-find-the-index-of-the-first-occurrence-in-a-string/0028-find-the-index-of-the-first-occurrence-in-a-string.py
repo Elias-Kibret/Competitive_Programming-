796e2d14bd0da7ad6bd6c521df6b1847c3021b44
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-06-30 21:43:59 -0500

    Time: 29 ms (89.76%), Space: 16.4 MB (69.35%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,50 +1,17 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        # found=False
-
-        
-        # follow_ptr=0
-
-        # check_ptr=0
-
-        # while check_ptr<len(haystack):
-        #     if haystack[check_ptr]==needle[follow_ptr]:
-        #         while follow_ptr<len(needle) and check_ptr<len(haystack):
-        #             if haystack[check_ptr]==needle[follow_ptr]:
-        #                 follow_ptr+=1
-                        
-        #             else:
-             
-        #                 follow_ptr=0
-        #                 break
-        #             check_ptr+=1
-                
-                
-        #     else:
-        #         check_ptr+=1
-        #     if len(needle)==follow_ptr:
-        #         return check_ptr-follow_ptr
-        # return -1
         if len(haystack)<len(needle):
             return -1
 
+        for i in range(len(haystack)-len(needle)+1):
+            j=0
 
-
-        index=[]
-
-        for i in range(len( haystack)):
-            if haystack[i]==needle[0]:
-                index.append(i)
-        for val in index:
-            ptr=0
-            check_ptr=val
-
-            while ptr<len(needle) and check_ptr<len(haystack) and haystack[check_ptr]==needle[ptr] :
-                ptr+=1
-                check_ptr+=1
-            if ptr==len(needle):
-                return val
+            while j<len(needle) and haystack[i+j]==needle[j]:
+                j+=1
+            if j==len(needle):
+                return i
         return -1
+
 
 
             
