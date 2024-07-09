13ed89e0dda4858eec706f228df3621fcb8a942f
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-07-08 22:49:48 -0500

    Time: 772 ms (22.34%), Space: 57 MB (85.22%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.py
+++ 1803-average-waiting-time/1803-average-waiting-time.py
@@ -1,15 +1,15 @@
-
-
 class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
-        timeChefAt = 0
-        waitingTime = 0
+
+        timeChefAt=0
+        waitingTime=0
 
         for customer in customers:
-            arrival, cookTime = customer
-            if timeChefAt < arrival:
-                timeChefAt = arrival
-            waitingTime += timeChefAt + cookTime - arrival
-            timeChefAt += cookTime
+            arrival, cooking=customer
 
-        return waitingTime / len(customers)
+            if timeChefAt<arrival:
+                timeChefAt=arrival
+            waitingTime+=timeChefAt+cooking-arrival
+            timeChefAt+=cooking
+        return waitingTime/len(customers)
+        
