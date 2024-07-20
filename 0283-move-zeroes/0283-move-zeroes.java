7ea9a8617e076f47a36af16bf871dce2061c1d61
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-07-20 04:56:16 -0500

    Time: 2 ms (82.72%), Space: 46.2 MB (28.05%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -4,12 +4,9 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if( nums[i]!=0){
                 int temp=nums[l];
-                nums[l]=nums[i];
+                nums[l++]=nums[i];
                 nums[i]=temp;
-                l++;
             }
-            
-        
         }
     }
 }
