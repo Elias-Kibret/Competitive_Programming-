9dbf1ea40721a2ea5b0ca8e364bda0ebc7515b39
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-06 15:42:03 -0500

    Time: 254 ms (11.03%), Space: 17.7 MB (8.15%) - LeetHub

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,57 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        x=Counter(word).most_common()
+        y=Counter(word)
+        print(x)
+
+        count=2
+        d=defaultdict(list)
+        
+        for val,c in x:
+            print(val)
+            if count not in d:
+                d[count]=[val]
+            else:
+                d[count].append(val)
+            count+=1
+            if count==10:
+                count=2
+        print(d)
+        res=0
+        for key in d.keys():
+            
+            # print(d[key])
+            for index,val in enumerate(d[key]):
+                
+                res+=(index+1)*y[val]
+        return res
+
+            
+        
+        
+        
+
+        
+
+        
+       
+
+        
+                
+                    
+                
+            
+      
+      
+
+      
+        
+            
+    
+    
+
+
+
+    
+        
+        
