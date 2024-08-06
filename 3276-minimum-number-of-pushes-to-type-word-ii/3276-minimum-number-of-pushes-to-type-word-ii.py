83721d24e8b4a95e0ce144fb3e6299c9e26ac849
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-06 16:45:59 -0500

    Time: 136 ms (94.48%), Space: 17.6 MB (36.93%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,30 +1,38 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        x=Counter(word).most_common()
-        y=Counter(word)
-        print(x)
+        vs=sorted(Counter(word).values(), reverse=True)
+        total=0
+        for index,val in enumerate(vs):
+            total+=val*(index//8+1)
+        return total
 
-        count=2
-        d=defaultdict(list)
+
+
+
+
+        # x=Counter(word).most_common()
+        # y=Counter(word)
+        # print(x)
+
+        # count=2
+        # d=defaultdict(list)
         
-        for val,c in x:
-            print(val)
-            if count not in d:
-                d[count]=[val]
-            else:
-                d[count].append(val)
-            count+=1
-            if count==10:
-                count=2
-        print(d)
-        res=0
-        for key in d.keys():
-            
-            # print(d[key])
-            for index,val in enumerate(d[key]):
+        # for val,c in x:
+        #     print(val)
+        #     if count not in d:
+        #         d[count]=[val]
+        #     else:
+        #         d[count].append(val)
+        #     count+=1
+        #     if count==10:
+        #         count=2
+        # print(d)
+        # res=0
+        # for key in d.keys():
+        #     for index,val in enumerate(d[key]):
                 
-                res+=(index+1)*y[val]
-        return res
+        #         res+=(index+1)*y[val]
+        # return res
 
             
         
