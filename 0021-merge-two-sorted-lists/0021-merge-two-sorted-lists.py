f75b6298ca1b1b6e9c9f3f22f283cf6c3d298241
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-09 21:34:19 -0500

    Time: 40 ms (45.51%), Space: 16.4 MB (74.94%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,32 +1,15 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        listNode=ListNode()
-        temp=listNode
-
-        while list1 and list2:
-            if list1.val==list2.val:
-                temp.next=ListNode(list1.val)
-                temp=temp.next
-                temp.next=ListNode(list2.val)
-                
-                list2=list2.next
-                list1=list1.next
-            elif list1.val<list2.val:
-                temp.next=ListNode(list1.val)
-                list1=list1.next
+        cur = dummy = ListNode()
+        while list1 and list2:               
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
             else:
-                temp.next=ListNode(list2.val)
-                list2=list2.next
-            temp=temp.next
-        if list1:
-            temp.next=list1
-        else:
-            temp.next=list2
-        print(listNode.next)
-        return listNode.next
-        
+                cur.next = list2
+                list2, cur = list2.next, list2
+                
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+            
+        return dummy.next
