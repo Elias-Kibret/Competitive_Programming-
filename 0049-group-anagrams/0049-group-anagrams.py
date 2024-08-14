381719a3156f07b3e882e93ece7be0d0142f677c
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-14 15:30:42 -0500

    Time: 92 ms (47.7%), Space: 19.4 MB (93.82%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,25 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map=defaultdict(list)
+        for val in strs:
+            key="".join(sorted(val))
+            
+            if key not in hash_map:
+                hash_map[key]=[val]
+            else:
+                hash_map[key].append(val)
+        return [val for val in hash_map.values()]
+           
+       
+            
+        
+
+      
+
+                    
+        
+        
+
+
+                    
+
