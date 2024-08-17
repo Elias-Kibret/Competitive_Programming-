89755251dcb857c32d4b7ba8bedac6879a95fffd
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-16 21:21:45 -0500

    Time: 82 ms (87.92%), Space: 20.2 MB (51.35%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram_dict = defaultdict(list)
-        for strg in strs:
-            sorted_strg="".join(sorted(strg))
-            anagram_dict[sorted_strg].append(strg)
-        return anagram_dict.values()
+        hash_map=defaultdict(list)
+
+        for val in strs:
+            key="".join(sorted(val))
+            if key not in hash_map:
+                hash_map[key]=[val]
+            else:
+                hash_map[key].append(val)
+        return hash_map.values()
+
+       
 
 
                     
