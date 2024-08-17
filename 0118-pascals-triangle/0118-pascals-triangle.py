698b0ed25439d525a9e39b6c533ecea29ed23dc6
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-16 21:49:58 -0500

    Time: 31 ms (89.12%), Space: 16.4 MB (97.54%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -3,13 +3,9 @@ class Solution:
         if numRows==0:
             return []
         res=[[1]]
-
         for numRow in range(2,numRows+1):
             result=[1]*numRow
-
             for i in range(1,numRow-1):
-                if i>len(res[-1]):
-                    break
                 result[i]=res[-1][i-1]+res[-1][i]
             res.append(result)
         return res
