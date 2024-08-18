f2a1876d9a07c40acb313413d02faa4e61af6344
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-18 14:59:22 -0500

    Time: 743 ms (30.66%), Space: 31.2 MB (49.89%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.py
+++ 0932-monotonic-array/0932-monotonic-array.py
@@ -1,36 +1,15 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        # increasingStack=[nums[0]]
-        # decreasingStack=[nums[0]]
 
-
-        # for i in range(1,len(nums)):
-        #     if nums[i]>=decreasingStack[-1] and nums[i]>=increasingStack[-1]:
-        #         increasingStack.append(nums[i])
-        #     if nums[i]<=increasingStack[-1] and nums[i]<=decreasingStack[-1]:
-        #         decreasingStack.append(nums[i])
-        # print(increasingStack,decreasingStack)
-    
-        # return len(increasingStack)==len(nums) or len(decreasingStack)==len(nums) 
-
-        if len(nums)<2:
-            return True
-        
-        direction=0
+        inc,dec=True,True
 
         for i in range(1,len(nums)):
-            if nums[i]>nums[i-1]:
-                if direction==0:
-                    direction=1
-                elif direction==-1:
-                    return False
-            elif nums[i]<nums[i-1]:
-                if direction==0:
-                    direction=-1
-                elif direction==1:
-                    return False
-        return True
-
+            if nums[i]<nums[i-1]:
+                inc=False
+            elif nums[i]>nums[i-1]:
+                dec=False
+        return inc or dec
+       
 
         
         
