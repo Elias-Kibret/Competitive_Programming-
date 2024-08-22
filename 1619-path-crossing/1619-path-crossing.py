4322f4e42ba2c67d411eed869df3c8c767018613
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-21 20:17:50 -0500

    Time: 37 ms (46.34%), Space: 16.5 MB (73.79%) - LeetHub

--- 1619-path-crossing/1619-path-crossing.py
+++ 1619-path-crossing/1619-path-crossing.py
@@ -29,16 +29,38 @@ class Solution:
         #         return True
         # return False
 
-        visit=set()
-        x,y=0,0
+        # visit=set()
+        # x,y=0,0
 
-        for d in path:
-            visit.add((x,y))
+        # for d in path:
+        #     visit.add((x,y))
 
-            dx,dy=direction[d]
+        #     dx,dy=direction[d]
 
-            x,y=x+dx,y+dy
+        #     x,y=x+dx,y+dy
 
-            if (x,y) in visit:
+        #     if (x,y) in visit:
+        #         return True
+        # return False
+
+
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        curr = [0, 0]
+        visit = {tuple(curr)}
+        
+        for p in path:
+            if p == "N":
+                curr[0] += 1
+            elif p == "S":
+                curr[0] -= 1
+            elif p == "E":
+                curr[1] += 1
+            else:
+                curr[1] -= 1
+            if tuple(curr) not in visit:
+                visit.add(tuple(curr))
+            else:
                 return True
+        
         return False
