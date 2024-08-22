13fa2680c1df0946a90ef70250748742a8611478
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-21 20:38:26 -0500

    Time: 146 ms (22.52%), Space: 17.9 MB (54.01%) - LeetHub

--- 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.py
+++ 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
-        if len(nums)<4:
-            return 0
+       
         nums.sort()
 
         return (nums[-1]*nums[-2])-(nums[0]*nums[1])
