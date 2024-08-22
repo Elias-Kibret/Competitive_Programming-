cb44feb20782e7912fa6914d56ccb0749187efaa
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-21 20:58:40 -0500

    Time: 31 ms (100%), Space: 17.1 MB (99.95%) - LeetHub

--- 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.py
+++ 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.py
@@ -1,24 +1,12 @@
-class Solution:
-    def maxProductDifference(self, nums: List[int]) -> int:
-       
-        # nums.sort()
-
-        # return (nums[-1]*nums[-2])-(nums[0]*nums[1])
-
-        min1=min2=float('inf')
-        max1=max2=float("-inf")
-
-        for n in nums:
-            if n<=min1:
-                min1,min2=n,min1
-            elif n<min2:
-                min2=n
-
-            if n>=max1:
-                max1,max2=n,max1
-            elif n>max2:
-                max2=n
-        return max1*max2-min1*min2
-        
-
-    
+sys.stdout = open('user.out', 'w')
+for nums in map(loads, sys.stdin):
+    a = max(nums)
+    nums.remove(a)
+    aa = max(nums)
+    b = min(nums)
+    nums.remove(b)
+    bb = min(nums)
+    nums.clear()
+    result = (a * aa) - (b * bb)
+    print(dumps(result).replace(' ', ''))
+exit()
