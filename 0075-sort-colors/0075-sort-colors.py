e4a492ccea6a34e62ecc253e57f65f3829862af7
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-23 18:24:57 -0500

    Time: 32 ms (85.96%), Space: 16.6 MB (22.06%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,15 +4,20 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         low,mid,high=0,0,len(nums)-1
+        map_counter=Counter(nums)
 
-        while mid<=high:
-            if nums[mid]==0:
-                nums[low],nums[mid]=nums[mid],nums[low]
-                low+=1
-                mid+=1
-            elif nums[mid]==1:
-                mid+=1
-            else:
-                nums[mid],nums[high]=nums[high],nums[mid]
-                high-=1
+        # while mid<=high:
+        #     if nums[mid]==0:
+        #         nums[low],nums[mid]=nums[mid],nums[]
+        j=0
+
+        for i in range(3):
+            while map_counter[i]!=0:
+                nums[j]=i
+                map_counter[i]-=1
+                j+=1
+
+           
+        print(nums)
+
                 
