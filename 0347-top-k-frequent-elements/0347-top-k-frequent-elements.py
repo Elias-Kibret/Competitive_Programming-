51b4c2d6b715c02770701c5cb211173ac34d1adb
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-22 20:52:21 -0500

    Time: 95 ms (25.06%), Space: 21.9 MB (24.59%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,40 +1,42 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        nums_count=Counter(nums)
-        if k>len(nums_count):
-            return []
+        # nums_count=Counter(nums)
+        # if k>len(nums_count):
+        #     return []
         
-        sorted_items = sorted(nums_count.items(), key=lambda x: x[1])[len(nums_count)-k:]
-        res=[]
+        # sorted_items = sorted(nums_count.items(), key=lambda x: x[1])[len(nums_count)-k:]
+        # res=[]
 
-        for val in sorted_items:
-            res.append(val[0])
+        # for val in sorted_items:
+        #     res.append(val[0])
+
+
+        # return res
+
+        
+        count={}
+        freq=[[] for _ in range(len(nums)+1)]
+        
+
+
+        for num in nums:
+            count[num]=1+count.get(num,0)
+        for val,index in count.items():
+            freq[index].append(val)
+        res=[]
+    
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                res.append(n)
+            if len(res)==k:
+                return res
         return res
 
 
-        # count={}
 
-        # freq=[[] for _ in range(len(nums)+1)]
-        
-        # for n in nums:
-        #     count[n]=1+count.get(n,0)
-        # for n,c in count.items():
-        #     freq[c].append(n)
-        # print(freq)
+       
 
-        # res=[]
-
-        # for i in range(len(freq)-1,0,-1):
-        #     for n in freq[i]:
-        #         res.append(n)
-        #         if len(res)==k:
-        #             return res
-        # return res
-
-
-
-
-
-        
-
-        
+       
+       
+       
+       
