603cc1fa2ffe9d630447d56a1961774c969ee0d2
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-23 19:01:51 -0500

    Time: 39 ms (45.35%), Space: 16.4 MB (92.76%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -2,22 +2,43 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        
         """
+
+
+    # Brutal Force
+        # map_counter=Counter(nums)
+        # j=0
+        # for i in range(3):
+        #     while map_counter[i]!=0:
+        #         nums[j]=i
+        #         map_counter[i]-=1
+        #         j+=1
+
+
+
+ 
+    #  Optimazed way
+
         low,mid,high=0,0,len(nums)-1
-        map_counter=Counter(nums)
 
-        # while mid<=high:
-        #     if nums[mid]==0:
-        #         nums[low],nums[mid]=nums[mid],nums[]
-        j=0
 
-        for i in range(3):
-            while map_counter[i]!=0:
-                nums[j]=i
-                map_counter[i]-=1
-                j+=1
+        while mid<=high:
+            if nums[mid]==0:
+                nums[mid],nums[low]=nums[low],nums[mid]
+                mid+=1
+                low+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
+            
 
-           
-        print(nums)
+        
+
+
+
+       
 
                 
