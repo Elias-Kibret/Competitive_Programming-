15272422afb4bc5a8b1f899151b1db10a69c5162
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-24 17:39:55 -0500

    Time: 187 ms (73.75%), Space: 17.6 MB (19.44%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
@@ -1,23 +1,28 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        first_occurrence = [-1] * 26  # Track the first occurrence of each character
-        last_occurrence = [-1] * 26   # Track the last occurrence of each character
+
+        first_occurence=[-1]*26
+        last_occurence=[-1]*26
+
+        for index,char in enumerate(s):
+            idx=ord(char)-ord('a')
+
+            if first_occurence[idx]==-1:
+                first_occurence[idx]=index
+
+            last_occurence[idx]=index
+        print(first_occurence,last_occurence)
+        result=set()
         
-        # Traverse the string to fill first and last occurrence arrays
-        for i, char in enumerate(s):
-            idx = ord(char) - ord('a')
-            if first_occurrence[idx] == -1:
-                first_occurrence[idx] = i  # First time seeing this character
-            last_occurrence[idx] = i  # Update the last occurrence for this character
-        
-        result = set()
-        
-        # For each character, check the range between its first and last occurrence
         for i in range(26):
-            if first_occurrence[i] != -1 and last_occurrence[i] != -1 and last_occurrence[i] - first_occurrence[i] > 1:
-                # Get all unique characters between the first and last occurrence
-                middle_chars = set(s[first_occurrence[i] + 1:last_occurrence[i]])
-                for middle_char in middle_chars:
-                    result.add(chr(i + ord('a')) + middle_char + chr(i + ord('a')))
         
+            print(True)
+            if first_occurence[i]!=-1 and last_occurence[i]!=-1 and last_occurence[i]-first_occurence[i]>1:
+                middle=set(s[first_occurence[i]+1:last_occurence[i]])
+                print(middle)
+
+                for val in middle:
+                    result.add(chr(i+ord('a'))+val+chr(i+ord('a')))
         return len(result)
+
+       
