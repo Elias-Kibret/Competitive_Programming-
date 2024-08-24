aeddca3792e58f97a536c6679a402bb630917d01
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-24 15:19:26 -0500

    Time: 202 ms (71.66%), Space: 17.5 MB (27.92%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
@@ -1,48 +1,23 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-
-        # value_index=defaultdict(list)
-
-        # for index,char in enumerate(s):
-        #     if char not in value_index:
-        #         value_index[char]=[index]
-        #     else:
-        #         value_index[char].append(index)
-
-                
-                   
-
-        # result=set()
-
-      
-        # for val in value_index.values():
-        #     if len(val)>=2 and val[-1]-val[0]>1:
-        #         low=val[0]+1
-        #         tempResult=val[0]
-
-        #         while low<val[-1]:
-        #             tempResult=s[val[0]]+s[low]+s[val[-1]]
-        #             if tempResult not in result:
-        #                 result.add(tempResult)
-        #             low+=1
-        # return len(result)                      
-
-
-        result=set()
-
-        for char in range(26):
-            c=chr(ord('a')+char)
-
-            first=s.find(c)
-            last=s.rfind(c)
-
-            if first!=-1 and last!=-1 and last-first>1:
-                for middle in set(s[first+1:last]):
-                    result.add(c+middle+c)
-        return len(result)
-
-
-
-
-
+        first_occurrence = [-1] * 26  # Track the first occurrence of each character
+        last_occurrence = [-1] * 26   # Track the last occurrence of each character
         
+        # Traverse the string to fill first and last occurrence arrays
+        for i, char in enumerate(s):
+            idx = ord(char) - ord('a')
+            if first_occurrence[idx] == -1:
+                first_occurrence[idx] = i  # First time seeing this character
+            last_occurrence[idx] = i  # Update the last occurrence for this character
+        
+        result = set()
+        
+        # For each character, check the range between its first and last occurrence
+        for i in range(26):
+            if first_occurrence[i] != -1 and last_occurrence[i] != -1 and last_occurrence[i] - first_occurrence[i] > 1:
+                # Get all unique characters between the first and last occurrence
+                middle_chars = set(s[first_occurrence[i] + 1:last_occurrence[i]])
+                for middle_char in middle_chars:
+                    result.add(chr(i + ord('a')) + middle_char + chr(i + ord('a')))
+        
+        return len(result)
