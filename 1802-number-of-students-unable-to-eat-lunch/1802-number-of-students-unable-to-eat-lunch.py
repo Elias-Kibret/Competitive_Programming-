35a344e25b01a232d629d983e0c1fbb1e87f8618
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-28 10:06:08 -0500

    Time: 42 ms (41.63%), Space: 16.4 MB (77.17%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.py
@@ -1,14 +1,26 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-
-        res=len(students)
-        cnt=Counter(students)
-        for val in sandwiches:
-            if cnt[val]>0:
-                res-=1
-                cnt[val]-=1
-            else:
-                return res
-        return res
-     
-
+        studentZero = 0
+        studentOne = 0
+        
+        # Count the number of students who prefer 0 and 1
+        for student in students:
+            if student == 0:
+                studentZero += 1
+            elif student == 1:
+                studentOne += 1
+        
+        # Try to serve each sandwich in the stack
+        for sandwich in sandwiches:
+            if sandwich == 0:
+                if studentZero > 0:
+                    studentZero -= 1
+                else:
+                    return studentZero + studentOne
+            elif sandwich == 1:
+                if studentOne > 0:
+                    studentOne -= 1
+                else:
+                    return studentZero + studentOne
+        
+        return 0
