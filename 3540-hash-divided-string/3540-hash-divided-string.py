4fbf6973d499964cbca3de5f1dd7b13b5cf84c62
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-31 16:52:06 -0500

    Time: 68 ms (50%), Space: 16.9 MB (50%) - LeetHub

---
+++ 3540-hash-divided-string/3540-hash-divided-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def stringHash(self, s: str, k: int) -> str:
+        result=""
+        j=0
+        while j<len(s):
+            total=0
+            i=0
+            while i<k:
+                total+=ord(s[j])-ord("a")
+                j+=1
+                i+=1
+            print(total)
+            result+=chr(total%26+ord("a"))
+        
+        return result
+        
+
+
+        
