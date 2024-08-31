64d52714487ee4023435991913d12efa37c293a4
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-08-31 17:02:14 -0500

    Time: 47 ms (100%), Space: 16.4 MB (100%) - LeetHub

--- 3540-hash-divided-string/3540-hash-divided-string.py
+++ 3540-hash-divided-string/3540-hash-divided-string.py
@@ -1,18 +1,28 @@
 class Solution:
     def stringHash(self, s: str, k: int) -> str:
         result=""
-        j=0
-        while j<len(s):
-            total=0
-            i=0
-            while i<k:
-                total+=ord(s[j])-ord("a")
-                j+=1
-                i+=1
-           
-            result+=chr(total%26+ord("a"))
         
+        # while j<len(s):
+        #     total=0
+        #     i=0
+        #     while i<k:
+        #         total+=ord(s[j])-ord("a")
+        #         j+=1
+        #         i+=1
+           
+        #     result+=chr(total%26+ord("a"))
+        
+        # return result
+        prefix=0
+
+        for index,ch in enumerate(s):
+            prefix+=ord(ch)-97
+
+            if (index+1)%k==0:
+                result+=chr(97+prefix%26)
+                prefix=0
         return result
+
         
 
 
