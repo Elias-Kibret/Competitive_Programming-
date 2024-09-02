bd61fe66bea8e5cfcc8c398638a9d7974e25551f
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-09-02 11:14:18 -0500

    Time: 578 ms (51.45%), Space: 30.7 MB (19.92%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.py
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.py
@@ -1,11 +1,11 @@
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
-        mod=k%sum(0+x for x in chalk)
+        mod=k%sum(chalk)
      
         for index,value in enumerate(chalk):
             if mod-value<0:
                 return index
             mod-=value
         
-        return 1
+        return -1
         
