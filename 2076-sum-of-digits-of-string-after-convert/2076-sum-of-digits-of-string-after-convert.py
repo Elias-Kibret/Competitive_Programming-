648b7748579e3246dd2baaaaea0297877b55cf9c
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-09-02 21:05:13 -0500

    Time: 27 ms (97.93%), Space: 16.5 MB (37%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -1,37 +1,48 @@
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
-        # temp=""
-        # for val in s:
-        #     temp+=str((ord(val)-96))
-        # N=len(temp)
-        # temp=int(temp)
-        # while k>0:
-        #     res=0
-        #     for _ in range(N):
-               
-        #         res+=temp%10
-        #         temp=temp//10
-        #     temp=res
-        #     k-=1
-        # return res
-        converted=self.convert(s)
-        result=converted
-
-        for i in range(k):
-            result=self.getSum(result)
-        return int(result)
 
 
-    def convert(self,s):
-        converted=""
-        alphabet = "abcdefghijklmnopqrstuvwxyz"
-        for char in s:
-            converted+=str(alphabet.index(char)+1)
-        return converted
+        number_str="".join(str(ord(char)-ord('a')+1) for char in s)
+        print(number_str)
+
+        for _ in range(k):
+            number_str=str(sum(int(digit) for digit in number_str))
+        return int(number_str)
+
+
         
-    def getSum(self,converted):
-        result=0
-        for char in str(converted):
-            result+=int(char)
-        return str(result)
+    #     # temp=""
+    #     # for val in s:
+    #     #     temp+=str((ord(val)-96))
+    #     # N=len(temp)
+    #     # temp=int(temp)
+    #     # while k>0:
+    #     #     res=0
+    #     #     for _ in range(N):
+               
+    #     #         res+=temp%10
+    #     #         temp=temp//10
+    #     #     temp=res
+    #     #     k-=1
+    #     # return res
+    #     converted=self.convert(s)
+    #     result=converted
+
+    #     for i in range(k):
+    #         result=self.getSum(result)
+    #     return int(result)
+
+
+    # def convert(self,s):
+    #     converted=""
+    #     alphabet = "abcdefghijklmnopqrstuvwxyz"
+    #     for char in s:
+    #         converted+=str(alphabet.index(char)+1)
+    #     return converted
+        
+    # def getSum(self,converted):
+    #     result=0
+    #     for char in str(converted):
+    #         result+=int(char)
+    #     return str(result)
         
