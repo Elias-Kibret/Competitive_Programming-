8fc4049129619f711343d5d81587eb7406f2686d
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-09-08 10:05:13 -0500

    Time: 39 ms (49.78%), Space: 16.9 MB (22.72%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
@@ -28,9 +28,9 @@ class Solution:
 
         for i in range(k):
             result[i]=current
-            part_size=base_size + (1 if extra_nodes>0 else 0)
+            part_size=base_size + (1 if extra_nodes> 0 else 0)
 
-            for j in range(part_size):
+            for _ in range(part_size):
                 prev=current
                 current=current.next
             if prev:
@@ -38,32 +38,3 @@ class Solution:
             extra_nodes-=1
         return result
               
-
-        # res=[]
-        # if  k>size:
-        #     res=[1]*size+[0]*(k-size)
-        # else:
-        #     temp=size//k
-        #     res=[temp]*k
-
-        # mod=size%k
-       
-        # index=0
-        # while mod>0 and k<size:
-        #     res[index]+=1
-        #     index+=1
-        #     mod-=1
-        # result=[]
-        # temp=head
-        
-        # for val in res:
-        #     curr_head=ListNode(0)
-        #     cur=curr_head
-            
-        #     for i in range(val):
-        #         cur.next=ListNode(temp.val)
-        #         temp=temp.next
-        #         cur=cur.next
-        #     result.append(curr_head.next)
-        # return result
-        
