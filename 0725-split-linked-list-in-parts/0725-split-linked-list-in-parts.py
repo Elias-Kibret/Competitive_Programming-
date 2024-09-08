b16c8bc3dd1fe0eafaece7a32cbb4572e972e50c
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-09-08 06:14:50 -0500

    Time: 37 ms (65.43%), Space: 16.9 MB (22.72%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
@@ -4,37 +4,66 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
-        l=0
+    def findLength(self,head:Optional[ListNode]) -> int:
+        size=0
         temp=head
         while temp:
-            l+=1
-            temp=temp.next    
-        res=[]
-        if  k>l:
-            res=[1]*l+[0]*(k-l)
-        else:
-            temp=l//k
-            res=[temp]*k
+            size+=1
+            temp=temp.next
+        return size
 
-        mod=l%k
-       
-        index=0
-        while mod>0 and k<l:
-            res[index]+=1
-            index+=1
-            mod-=1
-        result=[]
-        temp=head
-        
-        for val in res:
-            curr_head=ListNode(0)
-            cur=curr_head
-            
-            for i in range(val):
-                cur.next=ListNode(temp.val)
-                temp=temp.next
-                cur=cur.next
-            result.append(curr_head.next)
+
+    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+
+        size=self.findLength(head)
+
+        base_size=size//k
+        extra_nodes=size%k
+
+        result=[None]*k
+
+        current=head
+        prev=None
+
+
+        for i in range(k):
+            result[i]=current
+            part_size=base_size + (1 if extra_nodes>0 else 0)
+
+            for j in range(part_size):
+                prev=current
+                current=current.next
+            if prev:
+                prev.next=None
+            extra_nodes-=1
         return result
+              
+
+        # res=[]
+        # if  k>size:
+        #     res=[1]*size+[0]*(k-size)
+        # else:
+        #     temp=size//k
+        #     res=[temp]*k
+
+        # mod=size%k
+       
+        # index=0
+        # while mod>0 and k<size:
+        #     res[index]+=1
+        #     index+=1
+        #     mod-=1
+        # result=[]
+        # temp=head
+        
+        # for val in res:
+        #     curr_head=ListNode(0)
+        #     cur=curr_head
+            
+        #     for i in range(val):
+        #         cur.next=ListNode(temp.val)
+        #         temp=temp.next
+        #         cur=cur.next
+        #     result.append(curr_head.next)
+        # return result
         
