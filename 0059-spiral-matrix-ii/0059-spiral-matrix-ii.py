5fdc07bbafe7536a8b530c61edb1742a8fc0528f
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-09-08 23:35:33 -0500

    Time: 42 ms (15.79%), Space: 16.6 MB (53.12%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -1,12 +1,36 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
+        '''
+         we have n^2 elements 
+
+        '''
         N=n*n
         C,R=n,n
+
+        """
+        Whenever we have sparila matrix , we need 3 thing
+
+          1] pointer to track row 
+          2] pointer to track column
+          3] pointer to track direction
+
+        """
         i=0
         j=-1
         direction=1
+
+        '''
+         Intialise a matrix to hold result
+
+        '''
         matrix=[[-1]*n for _ in range(n)]
+
+        '''
+        Value we gonna set in the matrix 
+        '''
         value=1
+
+        
         while value<=N:
             for _ in range(C):
                 j+=direction
