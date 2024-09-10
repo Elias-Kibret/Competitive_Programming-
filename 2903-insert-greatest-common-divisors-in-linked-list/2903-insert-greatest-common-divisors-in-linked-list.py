1e21344a67a794f4dfdede4a303b03a00c7a84b7
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-09-10 07:39:14 -0500

    Time: 73 ms (47.04%), Space: 19.7 MB (10.35%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.py
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.py
@@ -4,20 +4,35 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        res=ListNode(0)
-        res_temp=res
-        temp=head
+    def gcd (self,a,b) -> int :
+        while b!=0:
+            a,b=b,a%b
+        return a
 
-        while temp:
-            res_temp.next=ListNode(temp.val)
-            res_temp=res_temp.next
-            if temp.next:
-                print(gcd(temp.val,temp.next.val))
-                res_temp.next=ListNode(math.gcd(temp.val,temp.next.val))
-                res_temp=res_temp.next
-            temp=temp.next
-        return res.next
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        
+        # res=ListNode(0)
+        # res_temp=res
+        # temp=head
+
+        # while temp:
+        #     res_temp.next=ListNode(temp.val)
+        #     res_temp=res_temp.next
+        #     if temp.next:
+        #         print(gcd(temp.val,temp.next.val))
+        #         res_temp.next=ListNode(self.gcd(temp.val,temp.next.val))
+        #         res_temp=res_temp.next
+        #     temp=temp.next
+        # return res.next
+
+        cur=head
+
+        while cur.next:
+            n1,n2=cur.val,cur.next.val
+            cur.next=ListNode(self.gcd(n1,n2),cur.next)
+            cur=cur.next.next
+        return head
             
         
                 
