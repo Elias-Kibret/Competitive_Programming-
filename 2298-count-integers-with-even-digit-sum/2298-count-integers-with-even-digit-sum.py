e3be7137a0b61fe6157ba6918bbfb3a9c2f0cdf6
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-09-12 11:42:31 -0500

    Time: 31 ms (97.19%), Space: 16.3 MB (99.69%) - LeetHub

--- 2298-count-integers-with-even-digit-sum/2298-count-integers-with-even-digit-sum.py
+++ 2298-count-integers-with-even-digit-sum/2298-count-integers-with-even-digit-sum.py
@@ -1,21 +1,35 @@
 class Solution:
-    def isEven(self,nums:str)->bool:
-
-        print(nums)
-        nums=sum(int(x) for x in nums)
-        return nums%2==0
         
     def countEven(self, num: int) -> int:
-
-        value=2
         count=0
 
-        while value<=num:
-            if self.isEven(str(value)):
+        for n in range(2,num+1):
+            digits, total=n,0
+
+            while digits:
+                digits,digit=divmod(digits,10)
+                total+=digit
+            if total%2==0:
                 count+=1
-            value+=1
         return count
 
+
+
+
+    #     value=2
+    #     count=0
+
+    #     while value<=num:
+    #         if self.isEven(str(value)):
+    #             count+=1
+    #         value+=1
+    #     return count
+
+    # def isEven(self,nums:str)->bool:
+
+    #     print(nums)
+    #     nums=sum(int(x) for x in nums)
+    #     return nums%2==0
 
 
         
