5379d860f0289595f5cfea63521a2d38c396de14
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-09-16 10:42:10 -0500

    Time: 78 ms (23.6%), Space: 19.7 MB (36.73%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.py
+++ 0539-minimum-time-difference/0539-minimum-time-difference.py
@@ -1,15 +1,46 @@
 class Solution:
-    def findMinDifference(self, timePoints: List[str]) -> int:
-        rebuild=[int(char[:2])*60 + int(char[3:]) for char in timePoints]
-        print(rebuild)
+    def convertToMinutes(self,time:str)->int:
+        hour,minutes=time.split(":")
+        return int(hour)*60 +int(minutes)
 
-        
+
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        '''
+        convert the HH:MM
+        to minutes 
+
+        '''
+
+        minutesPoint=[self.convertToMinutes(val) for val in timePoints]
+
+        # rebuild=[int(char[:2])*60 + int(char[3:]) for char in timePoints]
+    
+
+        '''
+        in one day we have 
+        24 hours and 
+        and in 1 hour we have 60 minutes
+
+        '''
         max_minutes=60*24
-        rebuild.sort()
-        mn=max_minutes
+
+        '''
+        we have to sort the minutes
+        why we have to sort 
+        because the minutes found in 
+        consquective manner 
+        '''
+        minutesPoint.sort()
+
+        '''
+        to find minimum of sth 
+        we need to make a first 
+        pointer maxiuum
+        '''
+        min_minutes=max_minutes
         
         
-        for i in range(1,len(rebuild)):
-            mn=min(mn,rebuild[i]-rebuild[i-1])
-        return min(mn,1440-rebuild[-1]+rebuild[0])
+        for i in range(1,len(minutesPoint)):
+            min_minutes=min(min_minutes,minutesPoint[i]-minutesPoint[i-1])
+        return min(min_minutes,max_minutes-minutesPoint[-1]+minutesPoint[0])
         
