a6f3caa8ad2c2962e505bf75a2b7942978ce8557
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-09-29 16:27:21 -0500

    Time: 0 ms (100%), Space: 40.3 MB (38.01%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,9 +1,22 @@
 class Solution {
     public int fib(int n) {
+        // if(n==0 || n==1){
+        //     return n;
+        // }
+        // return fib(n-1)+fib(n-2);
+
         if(n==0 || n==1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        int prev=0;
+        int cur=1;
+
+        for(int i=2;i<=n ;i++){
+            int temp=prev+cur;
+            prev=cur;
+            cur=temp;
+        }
+        return cur;
         
     }
 }
