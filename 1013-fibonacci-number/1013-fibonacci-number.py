82a3aedf4a3461763624f3397ffd917bae754011
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-09-29 16:30:02 -0500

    Time: 37 ms (59.83%), Space: 16.5 MB (60.09%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -2,7 +2,14 @@ class Solution:
     def fib(self, n: int) -> int:
         if n==0 or n==1:
             return n
-        else:
-           return self.fib(n-1) + self.fib(n-2)
+        # else:
+        #    return self.fib(n-1) + self.fib(n-2)
+        pre=0
+        cur=1
+
+        for i in range(2,n+1):
+            temp=cur+pre
+            pre,cur=cur,temp
+        return cur
         
         
