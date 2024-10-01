6c1e28e51f965917c2f549fe25dfbec7eb589c55
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-09-30 20:59:45 -0500

    Time: 1 ms (99.84%), Space: 43.3 MB (68.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,20 +3,36 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        HashMap<Character,Integer> count=new HashMap<>();
-        char[] strArray = s.toCharArray();
-        
-        for(char c: strArray){
-            count.put(c, count.getOrDefault(c, 0)+1);
-        }
-        strArray = t.toCharArray();
 
-        for(char c :strArray){
-            if(!count.containsKey(c) || count.get(c)==0){
-                return false;
+
+        int [] count=new int[26];
+        for(char c:s.toCharArray()){
+            count[c-'a']++;
+        }
+        for(char c:t.toCharArray()){
+            count[c-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i]!=0){
+              return false;
             }
-            count.put(c,count.get(c)-1);
         }
         return true;
+    //     HashMap<Character,Integer> count=new HashMap<>();
+    //     char[] strArray = s.toCharArray();
+        
+    //     for(char c: strArray){
+    //         count.put(c, count.getOrDefault(c, 0)+1);
+    //     }
+    //     strArray = t.toCharArray();
+
+    //     for(char c :strArray){
+    //         if(!count.containsKey(c) || count.get(c)==0){
+    //             return false;
+    //         }
+    //         count.put(c,count.get(c)-1);
+    //     }
+    //     return true;
+    // }
     }
 }
