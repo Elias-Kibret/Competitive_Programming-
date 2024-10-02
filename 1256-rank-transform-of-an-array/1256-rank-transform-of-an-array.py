28a04dd699a6cffafccc31dff3695d168bb7659e
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-10-01 21:48:56 -0500

    Time: 240 ms (64.41%), Space: 35.7 MB (45.49%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
@@ -18,11 +18,12 @@ class Solution:
         # return res
 
 
-        arr_sorted=sorted(set(arr))
+        # arr_sorted=sorted(set(arr))
 
-        mp={val:index+1 for index,val in enumerate(arr_sorted)}
+        # mp={val:index+1 for index,val in enumerate(arr_sorted)}
 
 
        
-        return [mp[val] for val in arr]
-        
+        # return [mp[val] for val in arr]
+        m: dict = {k: i + 1 for i, k in enumerate(sorted(set(arr)))}
+        return [m[n] for n in arr]
