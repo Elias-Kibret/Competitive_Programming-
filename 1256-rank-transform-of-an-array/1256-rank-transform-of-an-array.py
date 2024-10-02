2c1f254eb72dbfcafa7a2bb6c53731b5297ac8ac
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-10-01 21:47:10 -0500

    Time: 253 ms (30.89%), Space: 35.7 MB (45.49%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
@@ -1,19 +1,28 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
 
-        arr_sorted=sorted(arr)
-        mp={}
-        print(arr_sorted)
-        count=1
-        for index,key in enumerate(arr_sorted):
-            if key not in mp:
-                mp[key]=count
-                count+=1
+        # arr_sorted=sorted(arr)
+        # mp={}
+        # print(arr_sorted)
+        # count=1
+        # for index,key in enumerate(arr_sorted):
+        #     if key not in mp:
+        #         mp[key]=count
+        #         count+=1
             
 
-        res=[]
+        # res=[]
 
-        for num in arr:
-            res.append(mp[num])
-        return res
+        # for num in arr:
+        #     res.append(mp[num])
+        # return res
+
+
+        arr_sorted=sorted(set(arr))
+
+        mp={val:index+1 for index,val in enumerate(arr_sorted)}
+
+
+       
+        return [mp[val] for val in arr]
         
