ea29267d9da51f7b7dfdde2db1f4950b1e2afea1
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-10-17 18:40:52 -0500

    Time: 26 ms (96.87%), Space: 16.6 MB (20.9%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,15 +1,18 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n==0 or n==1:
-            return n
-        # else:
-        #    return self.fib(n-1) + self.fib(n-2)
-        pre=0
-        cur=1
 
-        for i in range(2,n+1):
-            temp=cur+pre
-            pre,cur=cur,temp
-        return cur
+        self.table=[-1]*(n+1)
+        return self.recDpFib(n)
         
+
+
+
+    def recDpFib(self,n):
+        print(self.table)
+        if self.table[n]<0:
+            if n==0 or n==1:
+                self.table[n]=n
+            else:
+                self.table[n]=self.recDpFib(n-2)+self.recDpFib(n-1)
+        return self.table[n]
         
