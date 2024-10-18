04cdad70521db66ba28c786b6a3b2d896dbbc462
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-10-18 12:03:55 -0500

    Time: 34 ms (72.59%), Space: 16.5 MB (20.9%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,18 +1,81 @@
 class Solution:
     def fib(self, n: int) -> int:
 
-        self.table=[-1]*(n+1)
-        return self.recDpFib(n)
+        '''
+        Bottom Up 
+        TC=O(n)
+        SC=O(1)
+        '''
+        if n<1:
+            return 0
+        
+        prev1=0
+        prev2=1
+
+        for i in range(2,n+1):
+            prev1,prev2=prev2,(prev1+prev2)
+        return prev2
+
+
+        '''
+        '''
+
+
+        '''
+        Bottom up 
+        Tabulation
+        TC=O(n)
+        SC=O(n) -> only the array space
+
+        '''
+        dp=[-1]*(n+1)
+        dp[0]=0
+        dp[1]=1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
+
+
+
+
+
+
+        '''
+        Top Down Approach
+        Memoizations
+        TC=O(n)
+        SC=O(n)+O(n)=O(n)
+        for space complexity we have a recusion space and array space which are O(n)
+        '''
+
+    
+    
+    
+    
+    #     self.table=[-1]*(n+1)
+    #     return self.recDpFib(n)
         
 
 
 
-    def recDpFib(self,n):
-        print(self.table)
-        if self.table[n]<0:
-            if n==0 or n==1:
-                self.table[n]=n
-            else:
-                self.table[n]=self.recDpFib(n-2)+self.recDpFib(n-1)
-        return self.table[n]
+    # def recDpFib(self,n):
+    #     print(self.table)
+    #     if self.table[n]<0:
+    #         if n==0 or n==1:
+    #             self.table[n]=n
+    #         else:
+    #             self.table[n]=self.recDpFib(n-2)+self.recDpFib(n-1)
+        # return self.table[n]
+
+    
+
+        
+        
+
+        
+            
+        
+
+
+
         
