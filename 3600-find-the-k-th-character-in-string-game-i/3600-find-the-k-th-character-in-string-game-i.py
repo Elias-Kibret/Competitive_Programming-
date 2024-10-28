54d2f23ed12a4c91c23d6085ab7a1576fe300cc6
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-10-28 12:03:05 -0500

    Time: 3 ms (85.71%), Space: 16.5 MB (56.8%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
@@ -1,22 +1,31 @@
 class Solution:
     def kthCharacter(self, k: int) -> str:
-        word="a"
-        while len(word)<k:
-            temp=""
-            for val in word:
-                temp+=chr(ord(val)+1)
-            word+=temp
-            # print(word)
-        return word[k-1]
+        return self.rec('a',k)
+    def rec(self,word:str,k:int) -> str:
+        if len(word)>=k:
+            return word[k-1]
+        temp=""
+        for val in word:
+            temp+=chr(ord(val)+1)
+        word+=temp
+        return self.rec(word,k)
+
+
+
+
+
+
+
+        # word="a"
+        # while len(word)<k:
+        #     temp=""
+        #     for val in word:
+        #         temp+=chr(ord(val)+1)
+        #     word+=temp
+          
+        # return word[k-1]
         
 
 
-    # def rec(self,word,k):
-    #     if len
-        
-
-    #     if len(s)
-
-
-    
+   
         
