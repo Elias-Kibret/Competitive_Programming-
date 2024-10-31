6aaa3c832f6fbe90599f68274189339b60da363e
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-10-31 11:32:45 -0500

    Time: 0 ms (100%), Space: 16.7 MB (19.57%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,21 +5,20 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        listNode=ListNode()
-        temp=listNode
+        # Base cases
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
 
-        while list1 and list2:
-            if list1.val<list2.val:
-                temp.next=ListNode(list1.val)
-                list1=list1.next
-            else:
-                temp.next=ListNode(list2.val)
-                list2=list2.next
-            temp=temp.next
-            
-        if list1 or list2:
-            temp.next=list1 if list1 else list2
-      
-        print(listNode.next)
-        return listNode.next
+        # Recursive case: compare the current nodes
+        if list1.val <= list2.val:
+            # list1's node is smaller, so include it in the result and move to the next node in list1
+            newNode = ListNode(list1.val)
+            newNode.next = self.mergeTwoLists(list1.next, list2)
+        else:
+            # list2's node is smaller, so include it in the result and move to the next node in list2
+            newNode = ListNode(list2.val)
+            newNode.next = self.mergeTwoLists(list1, list2.next)
         
+        return newNode
