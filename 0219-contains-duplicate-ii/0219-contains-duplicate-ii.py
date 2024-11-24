e952a0da782f0db83c6730a5cb97fa459353ea43
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-11-23 23:51:43 -0600

    Time: 20 ms (64.88%), Space: 30 MB (33.88%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,16 +1,10 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        hash_map={}
-
-
-        for index,val in enumerate(nums):
-            if val not in hash_map:
-                hash_map[val]=index
-                continue 
-            elif index-hash_map[val]<=k :
-                return True
-            hash_map[val]=index
-        return False
-                
+        hash_map = {}  # To store the last index of each element
         
+        for index, val in enumerate(nums):
+            if val in hash_map and index - hash_map[val] <= k:
+                return True  # Found a valid duplicate within the range
+            hash_map[val] = index  # Update the last index of the element
         
+        return False  # No valid duplicate found
