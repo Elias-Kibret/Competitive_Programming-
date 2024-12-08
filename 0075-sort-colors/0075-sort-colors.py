609ba2bc30c18949dadfa4b18892562df6f85305
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-12-08 17:10:08 -0600

    Time: 0 ms (100%), Space: 17 MB (15.94%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -2,53 +2,17 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        
         """
-        for i in range(len(nums)):
-            for j in range(len(nums)-1):
-                if nums[j]>nums[j+1]:
-                    nums[j],nums[j+1]=nums[j+1],nums[j]
-        
-        
-        
-            
-                
-
-    # Brutal Force
-        # map_counter=Counter(nums)
-        # j=0
-        # for i in range(3):
-        #     while map_counter[i]!=0:
-        #         nums[j]=i
-        #         map_counter[i]-=1
-        #         j+=1
+        low,mid,high=0,0,len(nums)-1
 
 
-
- 
-    #  Optimazed way
-
-        # low,mid,high=0,0,len(nums)-1
-
-
-        # while mid<=high:
-        #     if nums[mid]==0:
-        #         nums[mid],nums[low]=nums[low],nums[mid]
-        #         mid+=1
-        #         low+=1
-        #     elif nums[mid]==1:
-        #         mid+=1
-        #     else:
-        #         nums[mid],nums[high]=nums[high],nums[mid]
-        #         high-=1
-
-    # return nums
-            
-
-        
-
-
-
-       
-
-                
+        while mid<=high:
+            if nums[mid]==0:
+                nums[mid],nums[low]=nums[low],nums[mid]
+                mid+=1
+                low+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
