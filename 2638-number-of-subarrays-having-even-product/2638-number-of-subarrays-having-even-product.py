62e5b0b9806e2d8feaa989d8b00d14bb5a81e965
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2024-12-08 18:31:44 -0600

    Time: 39 ms (53.57%), Space: 28.8 MB (9.38%) - LeetHub

--- 2638-number-of-subarrays-having-even-product/2638-number-of-subarrays-having-even-product.py
+++ 2638-number-of-subarrays-having-even-product/2638-number-of-subarrays-having-even-product.py
@@ -3,17 +3,17 @@ from typing import List
 class Solution:
     def evenProduct(self, nums: List[int]) -> int:
         n = len(nums)
-        even_count = 0  # Counter for the number of subarrays with even product
-        last_even_index = -1  # Track the index of the last even number
+        even_count = 0  
+        last_even_index = -1  
 
         for i in range(n):
             if nums[i] % 2 == 0:
-                # Update the last even index when an even number is found
+              
                 last_even_index = i
-            # Add subarrays ending at index `i` that include the last even number
+       
             even_count += (last_even_index + 1)
 
         return even_count
 
-# Example Test Case
+
 
