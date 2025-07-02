ae8b00f3f2ec3d0da54373ed2a503d4ece38d8fa
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2025-07-01 21:23:14 -0500

    Time: 0 ms (100%), Space: 17.9 MB (29.91%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.py
+++ 3379-score-of-a-string/3379-score-of-a-string.py
@@ -1,14 +1,11 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        l=0
-        r=1
+
+
         total=0
-        while r<len(s):
-            total+=abs(ord(s[l])-ord(s[r]))
-            r+=1
-            l+=1
+
+        for i in range(1,len(s)):
+            total+=abs(ord(s[i])-ord(s[i-1]))
         return total
 
-
-         
         
