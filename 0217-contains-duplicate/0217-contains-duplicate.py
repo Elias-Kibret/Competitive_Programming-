97a04410a142a9d04ca9e949a56fb4a390a74e7e
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2025-09-27 15:08:21 -0500

    Time: 82 ms (5.18%), Space: 27.2 MB (92.49%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,20 +1,19 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums_set=set()
+        '''
+        so we are given Integer 
+        which means -10^9<=nums[i]<=10^9 --
+        '''
+        # for i in range(len(nums)-1):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]==nums[j]:
+        #             return True
+        # return False
 
-        for num in nums:
-            if num in nums_set:
+        nums.sort()
+        print(nums)
+
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
                 return True
-            nums_set.add(num)
-        return False
- 
-
-      
-        
-            
-        
-    
-      
-       
-        
-    
+        return False 
