704de5c726b443101967803c37d6810c6aaff1cc
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2025-11-11 13:25:40 -0600

    Time: 0 ms (100%), Space: 17.8 MB (43.45%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,25 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        index=len(nums1)-1
-        n=n-1
-        while n>=0:
-            nums1[index]=nums2[n]
-            n-=1
-            index-=1
+        merged_index=len(nums1)-1
+        for value in reversed(nums2):
+            nums1[merged_index]=value
+            merged_index-=1
         nums1.sort()
 
-       
-       
-       
-        
-        
-        
-        
+
 
     
-      
-            
-            
 
 
+        
