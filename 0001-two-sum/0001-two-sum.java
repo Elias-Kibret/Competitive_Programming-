b0fe83b05b8df4758ec3eb3a8fd885c3cf02eef0
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2026-02-25 09:12:19 -0500

    Time: 2 ms (99.2%), Space: 46.9 MB (84.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,14 @@
-class Solution{
-    public int [] twoSum(int [] nums, int target){
+class Solution {
+    public int [] twoSum(int [] nums,int target){
         Map<Integer,Integer> lookUp=new HashMap<>();
 
-        for (int index=0; index<nums.length;index++){
+        for (int index=0;index<nums.length;index++){
             int compliment=target-nums[index];
 
             if(lookUp.containsKey(compliment)){
-                return new int[] {index, lookUp.get(compliment)};
+                return new int [] {index,lookUp.get(compliment)};
             }
-            lookUp.put(nums[index], index);
+            lookUp.put(nums[index],index);
         }
         return new int [] {};
     }
