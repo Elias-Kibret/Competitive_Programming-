9ec17a408ca7e4c560fd68a4a197b0ce1b6671e2
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2026-03-05 10:14:38 -0500

    Time: 445 ms (85.07%), Space: 19.3 MB (90.77%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -3,20 +3,31 @@ class Solution:
         '''
         why we sort it 
         why we start from the reverse loop
+
         
         '''
 
         nums.sort()
     
         count=0
-
+        '''
+        why do we loop the array in reverse order 
+        why do we loop till 2 
+        '''
         for i in range(len(nums)-1,1,-1):
             left=0
             right=i-1
 
             while left<right:
                 if nums[left]+nums[right]>nums[i]:
+                    '''
+                    how do this count work like this 
+
+                    '''
                     count+=right-left
+                    '''
+                    why do we do this 
+                    '''
                     right-=1
                 else:
                     left+=1
