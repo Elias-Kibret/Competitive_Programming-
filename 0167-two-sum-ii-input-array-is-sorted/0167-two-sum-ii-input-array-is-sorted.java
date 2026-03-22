ebac38565985df15865746d7f95f244b7d136125
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2026-03-22 11:32:52 -0400

    Time: 2 ms (96.33%), Space: 48.4 MB (54.96%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,19 +3,21 @@ class Solution {
         int left=0;
         int right=numbers.length-1;
 
-        while(left<right){
-            if(numbers[left]+numbers[right]==target){
-                return new int []{left+1,right+1};
 
-            }
-            else if(numbers[left]+numbers[right]>target){
-                right--;
-            }
-            else{
-                left++;
-            }
+        while(left<right){
+           int twoSum=numbers[left]+numbers[right];
+           if(twoSum==target){
+            return new int [] {left+1,right+1};
+           }
+           else if(twoSum>target){
+            right--;
+           }else{
+            left++;
+           }
+
+
         }
         return new int []{};
-        
     }
+
 }
